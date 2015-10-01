1a79b9806a3c0765c4b9f6da9ebd7e82b98697dc
Avinash1992 <avinash.verma@globussoft.com>
2015-10-01 15:10:14 +0530

    websitereview

---
+++ Domain.Myfashion/Domain/WebSiteReviewData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Myfashion.Domain
+{
+    public class websitereviewdata
+   {
+       public Guid Id { get; set; }
+       public string websitename { get; set; }
+      public  string imageurl { get;set;}
+      public  string textname { get;set;}
+      public  string websitedescription { get;set;}
+      public  string GlobalRank { get;set;}
+      public  string CountryRank { get;set;}
+      public  string CategoryRank   { get;set;}
+      public  string VisitersOnSite {get;set;}
+      public  string TimeOnSite {get;set;}
+      public  string WebSitePageViewers  { get;set;}
+      public  string WebSiteBounceRate { get;set;}
+      public  string DirrectTrafficOnSite{ get;set;}
+      public  string ReferralTrafficOnSite {get;set;}
+      public  string SearchTrafficeOnSite{get;set;}
+      public  string SocialTrafficeOnSite{get;set;}
+      public  string MailTrafficeOnSite{get;set;}
+      public  string DisplayTrafficOnSite{get;set;}
+      public  string toprefersitedata  { get;set;}
+      public  string topdestiantionsites  { get;set;}
+      public  string PaidSearch { get;set;}
+      public  string organickeyword  { get;set;}
+      public  string paidkeyword {get;set;}
+      public  string socialsites{get;set;}
+      public  string sitesvalue   {     get;set;}
+      public  string display      {     get;set;}
+      public  string interestvalue    {     get;set;}
+      public  string audienceinterest   {     get;set;}
+      public  string visitedsites {     get;set;}
+      public  string similarwebsite   {     get;set;}
+      public string googleinmagesource { get; set; }
+      public string googleappname { get; set; }
+      public  string inmagesourceapp   {     get;set;}
+      public  string appnameapp     {     get;set;}
+      public string TrafficbyCountry { get; set; }
+   
+   }
+}
